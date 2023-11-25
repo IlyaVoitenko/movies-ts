6757cef326624f4423cc5666d5e543acbd843062
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2023-11-25 11:47:21 +0100

    added styled-components

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,16 +1,17 @@
 import { useSelector } from "react-redux";
 import { movieListSelector } from "../../store/selectors";
 import { Movie } from "../../interfaces";
+import { ContainerList } from "../../styles";
 import MovieItem from "../MovieItem";
 
 const MovieList = () => {
   const movieList = useSelector(movieListSelector);
   return (
-    <div>
+    <ContainerList>
       {movieList.map((movie: Movie) => {
         return <MovieItem key={movie.id} movie={movie} />;
       })}
-    </div>
+    </ContainerList>
   );
 };
 
